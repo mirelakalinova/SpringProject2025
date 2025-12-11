8ad53cd6fa43c9f0aa511343d2adc953307b48c9
mirelakalinova <119596034+mirelakalinova@users.noreply.github.com>
2025-12-11 18:38:46 +0200

    feat: added dashboard view

--- src/main/java/com/example/mkalinova/app/land/Controller/HomeController.java
+++ src/main/java/com/example/mkalinova/app/land/Controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.example.mkalinova.app.land.Controller;
 
+import com.example.mkalinova.app.order.service.OrderService;
+import com.example.mkalinova.app.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.stereotype.Controller;
@@ -9,12 +11,28 @@ import org.springframework.web.servlet.ModelAndView;
 @Slf4j
 @Controller
 public class HomeController extends BaseController {
+	private final OrderService orderService;
+	private final UserService userService;
 	
+	public HomeController(OrderService orderService, UserService userService) {
+		this.orderService = orderService;
+		this.userService = userService;
+	}
 	
 	@GetMapping("/")
 	public ModelAndView index() {
 		log.debug("Attempt to get index page..");
 		return super.view("fragments/index");
+		
+	}
+	@GetMapping("/dashboard")
+	public ModelAndView dashboard() {
+		log.debug("Attempt to get index page..");
+		ModelAndView modelAndView =  super.view("order/list");
+		modelAndView.addObject("orderList", orderService.getOrders(20));
+		modelAndView.addObject("heading","Последни поръчки");
+		
+		return modelAndView;
 		
 	}
 }
